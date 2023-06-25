072292ad8cc045a4d00a3dd2ff50a7e26edf866a
Andrius Mozūraitis <andriusmozuraitis@MacBook-PL71PV371T.local>
2023-06-25 07:33:21 +0300

    Published package

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,3 +1,14 @@
 import { buildProject } from "./utils/build.ts";
+import { execute } from "./utils/comand.ts";
+import { root } from "./utils/constants.ts";
+import { getPackageJson } from "./utils/package-json.ts";
 
+await execute("Bump version", "yarn version --minor");
 await buildProject();
+
+const packageJson = await getPackageJson();
+await execute(
+  "Publish",
+  `yarn publish --new-version ${packageJson.version}`,
+  root("dist")
+);
---
+++ scripts/utils/package-json.ts
@@ -0,0 +1,18 @@
+import { root } from "./constants.ts";
+
+export interface PackageJSON {
+  name: string;
+  private?: boolean;
+  version: string;
+  type?: string;
+  main?: string;
+  types: string;
+  dependencies: Record<string, string>;
+  license: string;
+  devDependencies?: Record<string, string>;
+}
+
+export async function getPackageJson(): Promise<PackageJSON> {
+  const packageJson = await Deno.readTextFile(root("package.json"));
+  return JSON.parse(packageJson) as PackageJSON;
+}
